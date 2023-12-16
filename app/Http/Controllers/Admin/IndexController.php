24fc638f3842ddec48a2b32e1368268138181ef9
Sultan-Orunov <osbfswd@gmail.com>
2023-12-17 01:31:55 +0500

    output of a number posts, categories, admins

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('admins.dashboard');
+        $postsCount = Post::all()->count();
+        $categoriesCount = Category::all()->count();
+        $adminsCount = Admin::all()->count();
+        return view('admins.dashboard', compact('postsCount', 'categoriesCount', 'adminsCount'));
     }
 }
--- resources/views/admins/dashboard.blade.php
+++ resources/views/admins/dashboard.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Posts</h5>
                     <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-                    <p class="card-text">number of posts: 8</p>
+                    <p class="card-text">number of posts: {{ $postsCount }}</p>
 
                 </div>
             </div>
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Categories</h5>
 
-                    <p class="card-text">number of categories: 4</p>
+                    <p class="card-text">number of categories: {{ $categoriesCount }}</p>
 
                 </div>
             </div>
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Admins</h5>
 
-                    <p class="card-text">number of admins: 3</p>
+                    <p class="card-text">number of admins: {{ $admins->count() }}</p>
 
                 </div>
             </div>
